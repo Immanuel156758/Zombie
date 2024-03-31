903eb0de9145f468717c3caecb1ab30279520e14
Immanuel156758 <161232399+Immanuel156758@users.noreply.github.com>
2024-03-31 17:06:29 +0200

    Add files via upload

---
+++ zombie.js
@@ -0,0 +1,15 @@
+AFRAME.registerComponent('zombie-fest',{
+	init:function () {
+		 var posX =-50;
+		 var posY =-3;
+		 var posZ =-150;
+		 far (var i = 1; i <=3; i++) {
+			var id ='zomb${i}';
+			var position ={x: posX, y: posY, z: posZ};
+			posX += 50;
+			this.zombieSpawn(id, position);
+			}
+		 },
+		 }
+		
+)
