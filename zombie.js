9dd9223e8603caeea5ef547e339139b584f21627
Immanuel156758 <161232399+Immanuel156758@users.noreply.github.com>
2024-04-07 16:41:13 +0200

    Add files via upload

--- zombie.js
+++ zombie.js
@@ -3,13 +3,25 @@ AFRAME.registerComponent('zombie-fest',{
 		 var posX =-50;
 		 var posY =-3;
 		 var posZ =-150;
-		 far (var i = 1; i <=3; i++) {
+		 for (var i = 1; i <=3; i++) {
 			var id ='zomb${i}';
 			var position ={x: posX, y: posY, z: posZ};
 			posX += 50;
 			this.zombieSpawn(id, position);
 			}
 		 },
+		 zombieSpawn: function (id, position){
+			var environEl = document.querySelector('a-scene');
+			var zombEl = document.createElement('a-entity');
+			zombEl.setAttribute('id', id);
+			zombEl.setAttribute('position', position);
+			zombEl.setAttribute('gltf-model', '#zombie');
+			zombEl.setAttribute('scale', '10, 10, 10');
+			zombEl.setAttribute('animation-mixer', {clip: 'Walk'});
+			zombEl.setAttribute('animation',{property:'position', to:'-10 0 1000', easing:'linear', loop:'true', dur:'200000'});
+			environEl.appendChild(zombEl);
+
 		 }
-		
+		}
 )
+		
